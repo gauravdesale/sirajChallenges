3e6dcb952d3a7df53ebc898788b6555006d7fb82
gauravdesale <desalegaurav60@gmail.com>
2017-10-08 23:56:34 -0500

    adding all these files

---
+++ scraperBasic.py
@@ -0,0 +1,6 @@
+import requests#anything web scraping stuff
+import re#perform expressions on the code
+from bs4 import BeautifulSoup#parse the data from html
+import operator#more readable
+import json
+import tabulate
